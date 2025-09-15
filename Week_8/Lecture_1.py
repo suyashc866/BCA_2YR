090e5427173a2125a7739a6263059d711387cec0
Suyash Chaudhary <suyashchaudhary@Suyashs-MacBook-Air.local>
2025-09-15 10:10:15 +0530

    week8lec1

---
+++ Week_8/Lecture_1.py
@@ -0,0 +1,131 @@
+# ===============================================================
+
+#       LECTURE : 1 -   List Practise Sheet
+
+
+# ===============================================================
+
+# SHEET  :- 4 (LIST) 
+
+
+# 1.Sum of list 
+# Given an array compute the sum of all elements. 
+# Input :-
+# A = [1 2 3 4 5] Output:
+# 15 
+
+
+# 2.Copy the Array 
+# You are given a constant array A and an integer B. 
+# You are required to return another array where Arr[i] = A[i] + B. 
+# Input :-
+# A = [1 2 3 2 1] B=3 
+# Output: [456 54] 
+
+
+# 3.Max and Min of an Array 
+# Take input an array A of size N and write a program to print maximum and minimum elements of the input array .Here N represents the length of the array .
+# Input :-
+# A = [1 2 3 4 5] 
+# Output: 51 
+
+
+
+# 4.Search Element 
+# You are given array A and an integer B. You have to tell whether B is present in array A or not.
+# Input:-
+# A = [1 5 9 1] 
+# B=5 Output: 1 
+
+
+
+# 5.Negative Integers 
+# Write a program to print all negative numbers from input array A of size N. 
+# Input:-
+# A = [1 -5 2 -8 -4] 
+# Output: -5
+# -8
+# -4 
+
+
+
+# 6.Even Odd Elements
+# For array A, you have to find the value of absolute difference between the counts of even and odd elements in the array.
+# Input:
+# A = [1 2 3 4]
+# Output:
+# 0 
+
+
+
+# 7.Separate Odd Even 
+# You are given an integer array A.
+# You have to print the odd and even elements of array A in 2 separate lines. 
+# Input: 
+# A = [1 2 3 4 5] 
+# Output: 
+# 135 24 
+
+
+# 8.Square of Array 
+# You are provided with an integer array A. Return another array B of size same as that of A such that B[i] = A[i]^2 
+# Input: 
+# A=[2, 6, 8, 1] 
+# Output: 
+# [4, 36, 64, 1] 
+
+
+
+# 9.Cube of Array 
+# You are provided with an integer array A. Return another array B of size same as that of A such that B[i] = A[i]^3
+# Input:
+# A=[2, 6, 8, 1] 
+# Output: 
+# [8, 216, 512, 1] 
+
+
+
+# 10.Reverse 
+# Given an array A, Find the reverse of it. (Solve this question with for loop) 
+# Input: 
+# A = [3, 5, 1, 2, 1, 2] 
+# Output: 
+# [2, 1, 2, 1, 5, 3] 
+
+
+# 11. Add two list element: 
+# Given two lists A1 and A2, each containing integers, write a Python program to compute the element-wise sum of the corresponding elements in the two lists and store the result in a new list.
+# Input: 
+# A1=[1, 2, 3,4] A2=[4, 5, 6,7] Output:
+# [5, 7, 9, 11] 
+
+
+# 12. Find the output : 
+# list = [2,4,6,8,10,12,14,16,18,20] print(list[:])
+# print(list[::])
+# print(list[2:5]) 
+# print(list[2:]) print(list[2::]) print(list[:2]) print(list[::2]) print(list[1::2]) print(list[2:10:2]) 
+
+
+# 13. Find the output : 
+# list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21] print(len(list))
+# print(list[-2 :-5: -1])
+# print(list[-2:]) 
+# print(list[-2::]) print(list[:-2]) print(list[::-2]) print(list[::-1]) 
+
+
+# 14. Find the output : 
+# mylist = [1.4, 2, 3, 4, 5, ‘Suyash’] mylist.reverse()
+# print(mylist) 
+# 15. Find the output : 
+# 	• ●  s = “Hello everyone how are you” print(s.split()) 
+# 	• ●  s = "Hello-everyone-how are you" 
+# print(s.split("-")) 
+# 	• ●  word = 'Suyash:Chaudhary:Noida' 
+# print(word.split(':')) 
+# 	• ●  t = “23456” print(t.split()) 
+# 	• ●  t="2345" print(t.split()) 
+
+# 16. Find the output : 
+# l1 = [1, 2, 3, 5, 8, 9] l2 = [3, 4, 5 , 6, 7, 10] result = l1 + l2 print(result) 
+# result1 = l1 * 3 print(result1) 
