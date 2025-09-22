cd6287e16fdba021a42ce2ec8a88a089d54a4e2c
Suyash Chaudhary <suyashchaudhary@Suyashs-MacBook-Air.local>
2025-09-22 14:24:09 +0530

    week9lec1

---
+++ Week_9/Lecture_1.py
@@ -0,0 +1,46 @@
+# ===============================================================
+
+#       LECTURE : 1 -   MCQ practice and Doubt Class for Codathon
+
+
+# ===============================================================
+
+
+
+# MCQ Questions  
+
+
+
+# HOW TO TAKE INPUT 
+
+# Input = int(input())
+
+# Numbers = list(map(int, input().split()))
+
+
+# Pattern Questions Logic 
+
+
+# Discuss  EXAM GUIDELINES 
+
+# BCA 2nd Year 
+
+# Basic Competitive Programming – CODETHON - 1 Exam Guidelines 
+# 	1. The exam will be held on 23rd September 2025 at the CSED Block (a few students in AB10). The seating plan will be shared on the exam day. 
+# 	2. Reporting Time: 10:00 AM | Exam Time: 10:30 AM – 12:30 PM. 
+# 	3. Students must bring their laptop with a charger (fully charged). 
+# 	4. The exam will be conducted on Coding Blocks. Register using your GLA login ID. The exam link will be shared via email. 
+# 	5. Exam Pattern: 5 coding questions (2 Easy – 5 marks each, 2 Medium – 5 marks each, 1 
+# Hard – 6. Rules: 
+# ○ ○ ○ ○ ○ ○ 
+# 10 marks). Duration: 2 hours. 
+# One-time login only.
+# No multiple tab switching or window resizing.
+# No AI tools (e.g., Co-pilot) or pre-installed extensions. No background applications (e.g., WhatsApp).
+# No pen, paper, or mobile phones allowed.
+# Cheating will lead to a UFM case and zero marks. 
+# 7. Students must report on time and follow all rules strictly. 
+
+
+
+
